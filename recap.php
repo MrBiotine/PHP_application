a3f032a784349ff919098b330e1c24360fbb27a3
bruno-PC\bruno <bruno.doue@laposte.net>
2023-07-19 17:36:35 +0200

    EndFirtsPart

---
+++ recap.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Récapitulatif des produits</title>
+</head>
+<body>
+    <?php
+    /*check if the key "products* does'nt exist or if it doesn't contain any data */
+    if(!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+        echo "<p>Aucun produit en session...</p>";        
+    }
+    else{
+        echo "<table>",
+                "<thead>",/* array header in HTML */
+                    "<tr>",
+                        "<th>#</th>",
+                        "<th>Nom</th>",
+                        "<th>Prix</th>",
+                        "<th>Quantité</th>",
+                        "<th>Total</th>",
+                    "</tr>",
+                "<thead>",
+            "<tbody>";
+        $totalGeneral = 0; /* initialize a new var $totalGeneral a t zero */
+        foreach($_SESSION['products'] as $index => $product){
+            echo "<tr>",
+                    "<td>".$index."</td>",
+                    "<td>"$product['name']."</td>",
+                    "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    "<td>"$product['qtt']."</td>",
+                    "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                "</tr>";
+                $totalGeneral += $product['total'] ;                      
+                             
+        }
+        echo "<tr>",
+        
+                "<td colspan=4>Total général : </td>",/*colspan=4 => merge the first 4 columns  */
+                "<td><strong>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+
+            "</tr>",
+
+
+        /* The number_format() function in PHP is used to format a numeric value with grouped thousands and decimal separators. 
+        
+        example : 
+        $number = 12345.6789;
+        $formattedNumber = number_format($number, 2, '.', ',');
+        echo $formattedNumber; // Output: 12,345.68
+
+        1.Number: The numeric value that you want to format.
+        2.Decimal Places: The number of decimal places to display in the formatted number.
+        3.Decimal Separator: The character used as the decimal separator.
+        4.Thousands Separator: The character used as the thousands separator.
+
+        */
+        echo "</tbody>",
+            "</table>";
+    }
+</body>
+</html>
---
+++ traitement.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+/*check if the key "submit" exist*/
+if(isset($_POST['submit'])){
+    /*we use a set of filters to filter, sanitanize, validate the data*/
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITANIZE_STRING);/*remove all special characters and  tags HTML, prevent injection code */
+    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);/*FILTER_FLAG_ALLOW_FRACTION add a rule to allow the use of ","(comma) or dot*/
+    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT); 
+
+    /*filter_input return only 3 values :
+    -the filter data
+    -or false, -or null */
+    if($name && $price && $qtt){
+
+    } 
+    
+    /*Associative Array "Key"(name) => "value" (input)*/
+    $product = [
+        "name" => $name,
+        "price" => $price,
+        "qtt" => $qtt,
+        "total" => $price*$qtt,
+    ];
+    /*store the array product in SG $_SESSION with the key 'products'*/
+    $_SESSION['products'][] = $product;
+
+}
+/*else perform a redirect to index.php */
+header("Location:index.php");
